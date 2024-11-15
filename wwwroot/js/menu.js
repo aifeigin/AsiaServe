51968d00d05b04181e14fe8d16844984cd181542
Oleksandr Feigin <12794937+aifeigin@users.noreply.github.com>
2024-11-15 16:35:57 +0200

    Added language menu, meta description, link alternates, redirects from the root to en pages

---
+++ wwwroot/js/menu.js
@@ -0,0 +1,25 @@
+﻿// Function to change the language when a new option is selected
+function changeLanguage() {
+    const languageSelector = document.getElementById('language-switcher');
+    const selectedLanguage = languageSelector.value;
+    
+    if (selectedLanguage) {
+        // Redirect the browser to the selected language's page
+        window.location.href = selectedLanguage;
+    } else {
+        console.error("No language URL is selected.");
+    }
+}
+
+window.onload = function () {
+    const currentPath = window.location.pathname;
+    const selector = document.getElementById('language-switcher');
+    if (selector) { 
+        for (const option of selector.options) {
+            if (currentPath.includes(option.value)) {
+                option.selected = true;
+                break;
+            }
+        }
+    }
+};
